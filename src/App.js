0ce50d4813d0ab9f40b7c25160794df27fc62946
bhagya2002 <bhagya02.p@gmail.com>
2021-07-30 15:34:35 -0600

    writing a new blog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./Pages/Home/Home";
+// import Home from "./Pages/Home/Home";
+// import Single from "./Pages/Single/Single";
+import Write from "./Pages/Write/Write";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Write />
     </>
   );
 }
