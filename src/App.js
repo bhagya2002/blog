edf504fb636bc8f001c9bfef70473ddac16a141f
bhagya2002 <bhagya02.p@gmail.com>
2021-07-31 12:35:05 -0600

    login and register page verison 1 complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
 // import Home from "./Pages/Home/Home";
 // import Single from "./Pages/Single/Single";
-import Write from "./Pages/Write/Write";
+// import Write from "./Pages/Write/Write";
+// import Settings from "./Pages/Settings/Settings";
+// import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Write />
+      <Register />
     </>
   );
 }
--- src/Pages/Write/Write.jsx
+++ src/Pages/Write/Write.jsx
@@ -1,5 +1,35 @@
 import "./write.css";
 
 export default function Write() {
-  return <div className="write">write</div>;
+  return (
+    <div className="write">
+      <img
+        className="write_image"
+        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <form className="write_form">
+        <div className="write_form-group">
+          <label htmlFor="fileInput">
+            <i class="write_icon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="write_input"
+            autoFocus={true}
+          />
+        </div>
+        <div className="write_form-group">
+          <textarea
+            placeholder="Enter your story..."
+            typeof="text"
+            className="write_input write_text"
+          ></textarea>
+        </div>
+        <button className="write_submit">Publish</button>
+      </form>
+    </div>
+  );
 }
